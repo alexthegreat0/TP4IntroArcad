d57c07fe8fc8c35b2a6e0f622f6240f27d66e3be
AlexTheGreat <alexandrewilbur@icloud.com>
2023-11-09 14:08:55 -0500

    Commit Initial

---
+++ main.py
@@ -0,0 +1,33 @@
+import arcade, random, time, colorama
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+COLORS = []
+
+
+
+class MyGame(arcade.Window):
+   def __init__(self):
+       super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
+       pass
+
+   def setup(self):
+       
+       pass
+
+   def on_draw(self):
+       arcade.start_render()
+
+       pass
+
+
+def main():
+   my_game = MyGame()
+   my_game.setup()
+
+   arcade.run()
+
+
+main()
+
